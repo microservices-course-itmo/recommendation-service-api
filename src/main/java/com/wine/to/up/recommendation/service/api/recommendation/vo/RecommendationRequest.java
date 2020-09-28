5c15a18ad7937a360fa5028a88bf8e9f541351c0
Ilya <ilyashin9@gmail.com>
2020-09-28 17:56:04 +0300

    change constructors modifiers in RecommendationRequest class

--- src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationRequest.java
+++ src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationRequest.java
@@ -1,17 +1,10 @@
 package com.wine.to.up.recommendation.service.api.recommendation.vo;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder(builderClassName = "Builder")
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public final class RecommendationRequest {
     private final Long id;
-
-    public static final class Builder {
-        private Builder(){}
-    }
 }

9ad947b57a8c4d875dbb02d8a47af03fe7010ac1
Ilya <ilyashin9@gmail.com>
2020-09-28 16:30:43 +0300

    add RecommendationRequest class

---
+++ src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationRequest.java
@@ -0,0 +1,17 @@
+package com.wine.to.up.recommendation.service.api.recommendation.vo;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder(builderClassName = "Builder")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RecommendationRequest {
+    private final Long id;
+
+    public static final class Builder {
+        private Builder(){}
+    }
+}

7e58ea1089815a355fc73f5ee9db2aaecaea2965
Roman Neviantsev <>
2020-09-29 20:52:22 +0300

    change response to have multiple ids

--- src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationResponse.java
+++ src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationResponse.java
@@ -3,8 +3,10 @@ package com.wine.to.up.recommendation.service.api.recommendation.vo;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder(builderClassName = "Builder")
 public final class RecommendationResponse {
-    private final Long id;
+    private final List<Long> recommendedIds;
 }

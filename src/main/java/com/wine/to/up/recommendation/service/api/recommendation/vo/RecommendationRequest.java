9c053eed945387c54c97c81eecca12b1b6017291
Kamran <kmirzaev@gmail.com>
2020-10-07 01:58:32 +0500

    added some javadoc comments

--- src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationRequest.java
+++ src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationRequest.java
@@ -3,8 +3,15 @@ package com.wine.to.up.recommendation.service.api.recommendation.vo;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * @version 1.1
+ * @author Alexander
+ */
 @Getter
 @Builder(builderClassName = "Builder")
 public final class RecommendationRequest {
+    /**
+     * id is an id of a current request
+     */
     private final Long id;
 }
--- src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationResponse.java
+++ src/main/java/com/wine/to/up/recommendation/service/api/recommendation/vo/RecommendationResponse.java
@@ -5,8 +5,15 @@ import lombok.Getter;
 
 import java.util.List;
 
+/**
+ * @version 1.1
+ * @author Roman
+ */
 @Getter
 @Builder(builderClassName = "Builder")
 public final class RecommendationResponse {
+    /**
+     * recommendedIds is a list of ids
+     */
     private final List<Long> recommendedIds;
 }
